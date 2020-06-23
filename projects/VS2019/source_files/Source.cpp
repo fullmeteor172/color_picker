e93fa87f88945acfee822b79043136e2c6c3cb6b
Dhruv <dhruvjha2002@gmail.com>
2020-06-23 11:48:02 +0530

    gradient square

--- projects/VS2019/source_files/Source.cpp
+++ projects/VS2019/source_files/Source.cpp
@@ -10,8 +10,13 @@ static const int SCALE_SCREEN = 1;
 static const int SCREEN_HEIGHT=500*SCALE_SCREEN;
 static const int SCREEN_WIDTH = 750*SCALE_SCREEN;
 
-int DrawGradient()
+Color sliderat = { 89,0,255,255 };
+Color transparent = { 0,0,0,0 };
+
+int DrawGradient(Color sliderColor)
 {
+    DrawRectangleGradientH(0,0,255,255, WHITE, sliderColor);
+    DrawRectangleGradientV(0, 0, 255, 255, transparent, BLACK);
     return 0;
 }
 
@@ -26,7 +31,7 @@ int GuiStart()
         ClearBackground(WHITE);
         //PlaceHolder for the slider
 
-        DrawGradient();
+        DrawGradient(sliderat);
         EndDrawing();
     }
     return 0;
